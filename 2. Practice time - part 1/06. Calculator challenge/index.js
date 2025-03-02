382b4dbcfb302bbceee5bd959aab89de3faf18f3
Shadman Sakib <sadmansakib10000@gmail.com>
2025-03-02 18:59:10 +0600

    calculator app

--- 2. Practice time - part 1/06. Calculator challenge/index.js
+++ 2. Practice time - part 1/06. Calculator challenge/index.js
@@ -1,7 +1,18 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = 0;
+let num2 = 0;
+
+const num1Input = document.getElementById("num1-el");
+const num2Input = document.getElementById("num2-el");
+
+//Realtime Listeners
+num1Input.addEventListener("input", () => {
+  //console.log(+num1Input.value);
+  num1 = +num1Input.value;
+});
+num2Input.addEventListener("input", () => {
+  //console.log(+num2Input.value);
+  num2 = +num2Input.value;
+});
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -11,4 +22,35 @@ document.getElementById("num2-el").textContent = num2
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
+const add = () => {
+  return num1 + num2;
+};
+const subtract = () => {
+  return num1 - num2;
+};
+const divide = () => {
+  return num1 / num2;
+};
+const multiply = () => {
+  return num1 * num2;
+};
 
+const add_btn = document.getElementById("add-btn");
+const sub_btn = document.getElementById("sub-btn");
+const mul_btn = document.getElementById("mul-btn");
+const div_btn = document.getElementById("div-btn");
+
+calcResult = document.getElementById("sum-el");
+
+add_btn.addEventListener("click", () => {
+  calcResult.textContent = add();
+});
+sub_btn.addEventListener("click", () => {
+  calcResult.textContent = subtract();
+});
+mul_btn.addEventListener("click", () => {
+  calcResult.textContent = multiply();
+});
+div_btn.addEventListener("click", () => {
+  calcResult.textContent = divide();
+});
