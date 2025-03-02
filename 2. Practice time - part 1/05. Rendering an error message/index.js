45f5b7478410ba6a0db122ab2452cb50a369e4e7
Shadman Sakib <sadmansakib10000@gmail.com>
2025-03-02 17:37:36 +0600

    practicing part 1 challenges

--- 2. Practice time - part 1/05. Rendering an error message/index.js
+++ 2. Practice time - part 1/05. Rendering an error message/index.js
@@ -2,4 +2,19 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+const errorMsg = () => {
+  document.querySelector("#error").textContent =
+    "Something went wrong, please try again";
+};
+document.querySelector("#purchase-btn").addEventListener("click", errorMsg);
 
+/*
+
+If errorMsg is not reusable, you can:
+
+document.querySelector("#purchase-btn").addEventListener("click", () => {
+    document.querySelector("#error").textContent =
+    "Something went wrong, please try again";
+});
+
+*/
